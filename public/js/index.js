7323b69347ee5538ffb0c0544ac18c2cfa1aae2b
kenzan8000 <kenzan8000@gmail.com>
2015-06-21 22:43:52 -0700

    add text editor

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,7 @@
+(function(global) {
+"use strict;"
+
+
 /**************************************************
  *            JokerDrone                          *
  **************************************************/
@@ -5,16 +9,42 @@ function JokerDrone() {
 };
 
 /// Member
+
+/// commands
+JokerDrone.prototype.commands = [];
+
 /**
  * deploy how Drones fly
  **/
 JokerDrone.prototype.deploy = JokerDrone_deploy;
+
+/**
+ * go
+ * @param position position from the start point ex) {x: -1.0, y: 1.0, z: 1.5}
+ **/
+JokerDrone.prototype.go = JokerDrone_go;
 
 /// Implementation
 function JokerDrone_deploy() {
     console.log("deploy");
 }
 
+function JokerDrone_go(position) {
+    this.commands.push({go: position});
+}
 
-/// Global variable
-var g_jokerDrone = new JokerDrone();
+
+/**************************************************
+ *                  Main                          *
+ **************************************************/
+window.onload = function() {
+    var editor = ace.edit("editor");
+    editor.getSession().setMode("ace/mode/javascript");
+    editor.setTheme("ace/theme/twilight");
+
+    /// Global variable
+    var g_jokerDrone = new JokerDrone();
+}
+
+
+})((this || 0).self || global);
